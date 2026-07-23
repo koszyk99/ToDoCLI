b1bab2e049e99021c41bf8be4526fe5d5e717262
koszyk99 <krzysztof_koszyk@interia.eu>
2026-07-23 16:56:17 +0200

    feat: create class TodoItem

---
+++ ToDoItem.cs
@@ -0,0 +1,20 @@
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class TodoItem
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime CompletedAt { get; set; }
+    public bool IsDone { get; set; }
+
+    public TodoItem(int id, string title)
+    {
+        ID = id;
+        Title = title;
+        CreatedAt = DateTime.Now;
+        CompletedAt = DateTime;
+        IsDone = false;
+    }
+}
